b6a45769e54a80b45b4003432c987d2374c149be
PaulMonaghanDexters <paulmonaghan@dexters.co.uk>
2024-11-01 14:29:05 +0000

    removed starts

--- Ch2 - Basics/helloworld_start.py
+++
@@ -1,8 +0,0 @@
-#
-# Example file for HelloWorld
-# LinkedIn Learning Python course by Joe Marini
-#
-print("hello world start")
-name = input("What is your name?")
-print("Nice to meet you",name)
-
